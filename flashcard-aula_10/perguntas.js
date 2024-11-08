2dd7ef762230eb1c712dcd28a40cb02bc17bdbd7
Leonardo241B2023 <141327330+Leonardo241B2023@users.noreply.github.com>
2024-11-08 09:37:38 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quem foi a primeira pessoa a viajar no Espaço??',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
